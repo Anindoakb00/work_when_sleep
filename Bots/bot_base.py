7b99669ef51e514b6d96fcccac9988c69d0b2217
Anindoakb <anindo.biswas.232@northsouth.edu>
2026-09-02 10:46:21 +0600

    Updated: bot_base.py.

--- Bots/bot_base.py
+++ Bots/bot_base.py
@@ -4,20 +4,17 @@ from selenium.webdriver.chrome.options import Options
 
 
 chrome_options = Options()
-scriptDirectory = pathlib.Path().absolute()
+project_directory = pathlib.Path(__file__).resolve().parents[1]
 chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--user-data-dir=chrome-data")
+chrome_options.add_argument(f"--user-data-dir={project_directory / 'userdata'}")
 chrome_options.add_argument('--profile-directory=Profile 8')
 prefs = {"profile.default_content_setting_values.notifications": 2}
 chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_argument('disable-infobars')
 chrome_options.add_experimental_option("useAutomationExtension", False)
 chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-chrome_options.add_argument("user-data-dir=chrome-data")
-# TODO: We have to solve the userdata problem it have to in one directory
-chrome_options.add_argument(f"user-data-dir={scriptDirectory}\\userdata")
 
 
 class BaseBot:
     def __init__(self):
-        self.driver = webdriver.Chrome("../chromedriver.exe", chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(options=chrome_options)
